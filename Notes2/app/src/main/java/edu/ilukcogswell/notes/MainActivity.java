8e016e84df4414bc18512d453761dd1a258921aa
Bella Luk <youisme610@gmail.com>
2017-04-10 13:56:06 -0700

    adding stuff to Notes
    >new activity for adding new notes
    >going back to main activity from new notes

--- Notes2/app/src/main/java/edu/ilukcogswell/notes/MainActivity.java
+++ Notes2/app/src/main/java/edu/ilukcogswell/notes/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.ilukcogswell.notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,12 +8,18 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private ArrayList<String> noteList;
     private ListView mainListView;
     private ArrayAdapter<String> listAdapter;
 
@@ -23,35 +30,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //finds listView
-
         mainListView = (ListView) findViewById(R.id.mainListView);
-
         String[] notes = new String[] {"something", "Another thing", "some other things"};
 
-        ArrayList<String> noteList = new ArrayList<String>();
+        noteList = new ArrayList<String>();
         noteList.addAll(Arrays.asList(notes));
 
+        readItems();
         //use this list to make array adapter
         listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, noteList);
 
-
-
         mainListView.setAdapter(listAdapter);
-
-
-
-
-
-
     }
 
-
+//adds item to the list?
     public void onAddItem(View v){
-        EditText etNewItem = (EditText) findViewById(R.id.quickNote);
+        EditText etNewItem = (EditText) findViewById(R.id.noteEditText);
         String itemText = etNewItem.getText().toString();
         listAdapter.add(itemText);
         etNewItem.setText("");
 
+    }
+
+
+    public void openNewNote(View view) {
+        Intent intent = new Intent(this, newNote.class);
+        //EditText editText = (EditText) findViewById(R.id.quickNote);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    private void readItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            noteList = new ArrayList<String>(FileUtils.readLines(todoFile));
+        }
+        catch(IOException e){
+            noteList = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, noteList);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ Notes2/app/src/main/java/edu/ilukcogswell/notes/newNote.java
@@ -0,0 +1,22 @@
+package edu.ilukcogswell.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class newNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_note);
+    }
+
+    //saves the new note to whatever we're doing
+    public void saveNote() {
+
+    }
+}
+
+
